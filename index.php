de9ed8363bb200ee113742543c68a3ee1bf82c44
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-09 13:35:44 +0200

    creation

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+        <title>PHP ex3.1 boucle</title>
+    </head>
+    <body >
+        <?php
+        for ($idx=0; $idx <10; $idx++)
+        {
+            echo ($idx." ");
+        }
+       ?>
+    </body>
+</html>
