77be2ca70e5b6e690af6bd62059991b1aa479582
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-09 15:05:50 +0200

    evo

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang='fr'>
 	<head>
-        <title>PHP ex3.1 boucle</title>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>PHP ex 3.1 boucle</title>
     </head>
     <body >
-        <?php
-        for ($idx=0; $idx <10; $idx++)
-        {
-            echo ($idx." ");
-        }
-       ?>
+        <p>
+            <?php
+            for ($idx=0; $idx <10; $idx++)
+            {
+                echo ($idx.' ');
+            }
+            ?>
+       </p>
     </body>
 </html>
